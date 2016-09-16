cff6e2e32363e3a16ffac0b6aab8526201d249f8
gschrock <gabrielschrock@gmail.com>
2016-09-16 11:52:52 -0700

    (feat) Set up Laravel/Homestead, MySQL migrations, and Angular

---
+++ informatics/resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Informatics</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Informatics</div>
+            </div>
+        </div>
+        
+        <!-- Angular -->
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+        
+    </body>
+</html>
